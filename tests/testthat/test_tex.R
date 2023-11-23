5ac9d66ecb5cbec46c9913e6bc35f0c24b6bf23f
Maximilian Pilz <maximilian.pilz@itwm.fraunhofer.de>
2023-11-23 09:19:56 +0100

    remove travis skip

--- tests/testthat/test_tex.R
+++ tests/testthat/test_tex.R
@@ -2,7 +2,7 @@
 
 test_that("knit a tex file with DescrTab2 tables", {
   skip_on_cran()
-  skip_on_travis()
+  skip_on_ci()
   suppressWarnings(expect_type(
     rmarkdown::render(
       "../rmds/tex_doc.Rmd",
